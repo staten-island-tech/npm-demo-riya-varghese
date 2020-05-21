09189070c7314bca9be15868594877d41f4ab367
riya-varghese <57456903+riya-varghese@users.noreply.github.com>
2020-05-20 22:21:13 -0400

    kk

---
+++ src/js/calc.js
@@ -0,0 +1,9 @@
+export const calc = {
+  add: function (x, y) {
+    return x + y;
+  },
+};
+/* export const DOMSelectors = {
+  header: document.querySelector(".header"),
+}; */
+//console.log(calc.add(2, 6));
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import { calc } from "./calc.js";
+//console.log(calc);
+import "aos/dist/aos.css";
+import AOS from "aos";
+AOS.init();
